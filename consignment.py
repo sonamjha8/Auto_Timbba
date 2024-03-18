b937921634bb7a8a5b6c70d5d2d099a58b700c85
Kumar7668 <sonumandal048@gmail.com>
2024-03-18 12:10:09 +0530

    configured project structure

--- consignment.py
+++ consignment.py
@@ -1,26 +1,20 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 
-  
 class Consignment:
+    def __init__(self, driver):
+        self.driver = driver
 
-    def cons_access():
-         #Click on New Pinewood Log
-         pinelog = driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[2]/section/div/div[2]/div/div[1]/div/a[3]')
-         pinelog.click()
+    def cons_access(self):
+        # Click on New Pinewood Log
+        pinelog = self.driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[2]/section/div/div[2]/div/div[1]/div/a[3]')
+        pinelog.click()
 
-         #Pinewood form open 
-         #click on consignment select dropdown 
-         element = driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[2]/section/div/div[4]/div[2]/div/section/form/div[1]/div[1]/div/div/span/select')
-         element.click()
+        # Pinewood form open
+        # click on consignment select dropdown
+        element = self.driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[2]/section/div/div[4]/div[2]/div/section/form/div[1]/div[1]/div/div/span/select')
+        element.click()
 
-         #select dropdown list
-         #element = self.driver.find_element_by_id("your_dropdown_id")
-         #select = Select(element)
-         element =select.select_by_value('659fbfbcb9474e11b520b4e3')
-
-        
+        # select dropdown list
+        # element = self.driver.find_element_by_id("your_dropdown_id")
+        # select = Select(element)
+        # element.select_by_value('659fbfbcb9474e11b520b4e3')
